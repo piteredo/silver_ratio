0a0bb691b78510c9ec879ef179bd4c56aff54cd2
piteredo <piteredo@hotmail.com>
2018-12-08 22:28:12 +0900

    adjust

--- main.js
+++ main.js
@@ -682,7 +682,7 @@ phina.define('Ball',{
             let highlightTime = 200;
             let waitCount = 1000 * 60 / (BPM / 4) - 120;
             this.tweener
-              .wait(this.WAIT_COUNT)
+              //.wait(this.WAIT_COUNT)
               .call(function(){
                 switch(Layout().getSquareDirection(this.layoutId)){
                   case "PORTRAIT":
